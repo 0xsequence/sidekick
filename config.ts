4b826c2260783eed090a4b195e3ec0d8e0bcf654
VGabriel45 <vgabrielmarian21@gmail.com>
2025-06-11 14:10:16 +0300

    remove config file

--- config.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs'
-import yaml from 'js-yaml'
-
-const config = yaml.load(
-	fs.readFileSync('sidekick.config.yml', 'utf8')
-) as Record<string, unknown>
-
-export default config
