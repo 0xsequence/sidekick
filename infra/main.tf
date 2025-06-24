9cf2b00c6631cff8aa4b18697df35f083d287300
andres-horizon <amonje@horizon.io>
2025-06-24 06:55:45 -0700

    No CD part

--- infra/main.tf
+++ infra/main.tf
@@ -200,38 +200,3 @@ module "ecs" {
   ecs_service_lb_container_port   = 7500
 }
 
-# OPENID cONNECT ***********************************************
-
-data "aws_caller_identity" "current" {}
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
-resource "aws_iam_role" "github_actions" {
-  name = "GitHubActionsDeployRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:0xsequence/sidekick:*"
-          }
-        }
-      }
-    ]
-  })
-}
-
