55f0bba3f296c334e2ba23369edff13a586c304d
andres-monje <amonje@horizon.io>
2025-09-23 20:09:02 -0500

    sidekick for prod

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-bucket-sidekick"
+    bucket       = "terraform-state-bucket-sidekick-prod"
     key          = "sidekick/terraform.tfstate"
     region       = "us-west-2"
     encrypt      = true
