5bda919dc1aa72782c934f48268334e3622ca345
andres-monje <amonje@horizon.io>
2025-09-25 13:36:35 -0500

    remove all unrelated infra changes to avoid unnecesary conflicts

--- src/plugins/bull/bull.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-import fp from 'fastify-plugin'
-import { createRewardQueue } from '~/queues/rewardQueue'
-
-export default fp(async (fastify: FastifyInstance) => {
-	const rewardQueue = createRewardQueue(fastify)
-	fastify.decorate('rewardQueue', rewardQueue)
-})
